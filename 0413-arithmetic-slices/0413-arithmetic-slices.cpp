489e0fd2faa3860a682efd61f17a7e576b1fe5f4
soloshowdown <ushinkarkunal@gmail.com>
2025-09-23 21:34:45 +0530

    Time: 0 ms (100%), Space: 10.9 MB (57.35%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.cpp
+++ 0413-arithmetic-slices/0413-arithmetic-slices.cpp
@@ -2,16 +2,19 @@ class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
         int n = nums.size();
-        int t = 0;
+        if (n < 3) return 0; 
+
+        int total = 0;
         int count = 0;
-        for(int i =2;i<n;i++){
-            if((nums[i] - nums[i-1]) == (nums[i-1] - nums[i-2])){
-                count ++;
-                t += count;
-            }else{
-                count = 0;
+
+        for (int i = 2; i < n; i++) {
+            if (nums[i] - nums[i-1] == nums[i-1]-nums[i-2]) {
+                count += 1;       
+                total += count;   
+            } else {
+                count = 0;       
             }
         }
-        return t;
+        return total;
     }
 };

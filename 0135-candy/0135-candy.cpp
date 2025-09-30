8befbbaf62602460003919ca0915af318a61df2b
soloshowdown <ushinkarkunal@gmail.com>
2025-09-30 12:53:46 +0530

    Time: 0 ms (100%), Space: 22.8 MB (63.93%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> candies(n, 1);  // sabko 1 candy initially
+
+        // Left to Right
+        for(int i=1; i<n; i++){
+            if(ratings[i] > ratings[i-1]){
+                candies[i] = candies[i-1] + 1;
+            }
+        }
+
+        // Right to Left
+        for(int i=n-2; i>=0; i--){
+            if(ratings[i] > ratings[i+1]){
+                candies[i] = max(candies[i], candies[i+1] + 1);
+            }
+        }
+
+        int total = 0;
+        for(int c : candies) total += c;
+
+        return total;
+    }
+};

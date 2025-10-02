eac5e7833165b0734c03d35d0eb6393194515bc4
soloshowdown <ushinkarkunal@gmail.com>
2025-10-02 21:05:44 +0530

    Time: 0 ms (100%), Space: 8.7 MB (57.24%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int n = haystack.size();
-        int m = needle.size();
-
-        for (int i = 0; i <= n - m; i++) {
-            if (haystack.substr(i, m) == needle) {
-                return i; // needle found at index i
+        int len_n=needle.size();
+        int len_h=haystack.size();
+        int i=0;
+        while(i<len_h)
+        {
+            int j=i;
+            if(haystack[i]==needle[0])//first ele match
+            {
+                string temp=haystack.substr(i,len_n);
+                if(temp==needle)//match found
+                {
+                    return i;
+                }
             }
+            i++;
         }
-        return -1; // needle not found
+        return -1;
     }
 };

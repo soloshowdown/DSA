8e558ddcb4dbc56e5e7efdfc84307c63930c73da
soloshowdown <ushinkarkunal@gmail.com>
2025-10-08 20:37:22 +0530

    Time: 3 ms (81.65%), Space: 10.4 MB (97.33%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        unordered_set<char> window; // store unique characters in current window
-        int left = 0, right = 0;    // window boundaries
-        int maxLen = 0;             // stores longest valid substring length
-        
-        while (right < s.size()) {
-            // If current character already in window -> shrink window from left
-            while (window.find(s[right]) != window.end()) {
-                window.erase(s[left]);
-                left++;
-            }
-            
-            // Add current character to window
-            window.insert(s[right]);
-            
-            // Update maximum length
-            maxLen = max(maxLen, right - left + 1);
-            
-            // Move right pointer to expand window
-            right++;
-        }
-        
-        return maxLen;
-    }
+   int lengthOfLongestSubstring(string s) {
+       vector<bool> a(95, false);
+       int ans = 0, l = 0, r = 0;
+       
+       while (r < s.size()) {
+           if ( !a[ s[r] - 32 ] ) {
+               ans = max(ans, r - l + 1);
+               a[ s[r++] - 32 ] = true;
+           }
+           else a[ s[l++] - 32 ] = false;
+       }
+
+       return ans;
+   }
 };

3f36d694cf004630ddebe1f7b217b3b3f7e527de
kunal ushinkar <ushinkarkunal@gmail.com>
2025-11-15 17:44:57 +0530

    Time: 3 ms (55.38%), Space: 32.8 MB (39.53%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
+
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return atMost(nums, goal) - atMost(nums, goal-1);
+
+        return atMost(nums, goal) - atMost(nums, goal - 1);
     }
-    int atMost(vector<int>& nums, int goal){
-        int head, tail = 0, sum = 0, result = 0;
-        for (head = 0; head < nums.size(); head++) {
-            sum += nums[head];
-            while (sum > goal && tail <= head) {
-                sum -= nums[tail];
-                tail++;
+
+private:
+
+    int atMost(vector<int>& nums, int k) {
+        // If k is negative, no such subarrays exist
+        if (k < 0) return 0;
+
+        int left = 0;
+        int sum = 0;
+        int count = 0;
+
+        // Traverse the array using right pointer
+        for (int right = 0; right < nums.size(); right++) {
+            sum += nums[right];
+
+            while (sum > k) {
+                sum -= nums[left];
+                left++;
             }
-            result += head - tail + 1;
+
+            count += (right - left + 1);
         }
-        return result;
+
+        return count;
     }
 };

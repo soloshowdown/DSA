71040227bef0852d5e021bf20c07364c49c1c988
kunal ushinkar <ushinkarkunal@gmail.com>
2025-11-20 21:47:01 +0530

    Time: 0 ms (100%), Space: 17 MB (58.93%) - LeetHub

---
+++ 1113-moving-stones-until-consecutive-ii/1113-moving-stones-until-consecutive-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+	vector<int> numMovesStonesII(vector<int>& stones) {
+		sort(stones.begin(),stones.end());
+		int n=stones.size();
+		int MAX=0;
+		int MIN=INT_MAX;
+		int a=stones.back()-stones[1]+1-(n-1);
+		int b=stones[n-2]-stones[0]+1-(n-1);
+		int j=0;
+		MAX=max(a,b);
+		for(int i=0;i<n;i++){
+			while(j<n && stones[j]-stones[i]+1<n){
+				j++;
+			}
+			if(j>=n){
+				break;
+			}
+			int gap;
+			if(stones[j]-stones[i]+1==n){
+				gap=n-(j-i+1);
+			}
+			else if(j-1-i+1==n-1){
+				gap=2;
+			}
+			else{
+				gap=n-(j-1-i+1);
+			}
+			MIN=min(MIN,gap);
+		}
+		return {MIN,MAX};
+	}
+};

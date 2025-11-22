7ed19e7ead459462e59e40ff572a286ce64323f6
kunal ushinkar <ushinkarkunal@gmail.com>
2025-11-22 23:36:34 +0530

    Time: 1 ms (97.18%), Space: 10.4 MB (84.94%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
@@ -1,27 +1,24 @@
 class Solution {
-public:
-    int balancedString(string s) {
-        int n = s.size(), target = n / 4;
-        unordered_map<char, int> freq;
-        for (char c : s) ++freq[c];
+ public:
+  int balancedString(string s) {
+    const int n = s.length();
+    const int k = n / 4;
+    int ans = n;
+    vector<int> count(128);
 
-        // Already balanced
-        if (freq['Q'] <= target && freq['W'] <= target &&
-            freq['E'] <= target && freq['R'] <= target) return 0;
+    for (const char c : s)
+      ++count[c];
 
-        int res = n, l = 0;
-        for (int r = 0; r < n; ++r) {
-            --freq[s[r]];  // remove from "outside"
-            while (l <= r &&
-                   freq['Q'] <= target &&
-                   freq['W'] <= target &&
-                   freq['E'] <= target &&
-                   freq['R'] <= target) {
-                res = min(res, r - l + 1);
-                ++freq[s[l]];  // restore left
-                ++l;
-            }
-        }
-        return res;
+    for (int i = 0, j = 0; i < n; ++i) {
+      --count[s[i]];
+      while (j < n && count['Q'] <= k && count['W'] <= k && count['E'] <= k &&
+             count['R'] <= k) {
+        ans = min(ans, i - j + 1);
+        ++count[s[j]];
+        ++j;
+      }
     }
+
+    return ans;
+  }
 };

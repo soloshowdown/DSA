e3df8dfa3c05fc267c48341b4a44773e7725d466
kunal ushinkar <ushinkarkunal@gmail.com>
2025-11-23 19:46:37 +0530

    Time: 1 ms (92.09%), Space: 74.9 MB (33.45%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        // Exactly k odds = at most k - at most (k-1)
-        return func(nums, k) - func(nums, k - 1);
-    }
-
-    // Counts subarrays with at most 'goal' odd numbers
-    int func(vector<int>& nums, int goal) {
-        if(goal < 0) return 0;  
-        int left = 0, right = 0, sum = 0, count = 0;
-
-        while(right < nums.size()) {
-            sum += (nums[right] % 2);  // Count odd numbers
-
-            // Shrink window if too many odds
-            while(sum > goal) {
-                sum -= (nums[left] % 2);
-                left++;
+        int n = nums.size();
+        vector<int> cnt(n + 1, 0);
+        cnt[0] = 1;
+        int ans = 0, t = 0;
+        for (int v : nums) {
+            t += v & 1;
+            if (t - k >= 0) {
+                ans += cnt[t - k];
             }
-
-            // All subarrays ending at right are valid
-            count += (right - left + 1);
-            right++;
+            cnt[t]++;
         }
-        return count;
+        return ans;
     }
 };

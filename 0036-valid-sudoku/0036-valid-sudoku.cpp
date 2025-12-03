06777fbb167039d54cf5e8b8e8b8ec23e744a268
kunal ushinkar <ushinkarkunal@gmail.com>
2025-12-03 20:02:15 +0530

    Time: 11 ms (14.14%), Space: 25.5 MB (19.35%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<unordered_set<char>> rows(9),cols(9),boxes(9);
-        
-        for(int r=0;r<9;r++){
-            for(int c=0;c<9;c++){
-                char val = board[r][c];
+        vector<unordered_set<int>> rowHash(9);
+        vector<unordered_set<int>> colHash(9);
+        vector<unordered_set<int>> boxHash(9);
+        for(int r = 0 ; r < 9 ; ++r) {
+            for(int c = 0 ; c < 9 ; ++c) {
+                if( board[r][c] == '.') continue;
+                if( rowHash[r].count( board[r][c] - '0' ) ) return false;
+                if( colHash[c].count( board[r][c] - '0' ) ) return false;
+                int boxId = r / 3 * 3 + c / 3;
+                if( boxHash[boxId].count( board[r][c] - '0' ) ) return false;
 
-                if( val=='.') continue; //for empty cell
-                int boxIndex =(r/3)*3 +(c/3);
-
-                if (rows[r].count(val) || cols[c].count(val) || boxes[boxIndex].count(val) ){
-                    return false;
-
-                }
-                   // insert
-                rows[r].insert(val);
-                cols[c].insert(val);
-                boxes[boxIndex].insert(val);
+                rowHash[r].insert( board[r][c] - '0' );
+                colHash[c].insert( board[r][c] - '0' );
+                boxHash[boxId].insert( board[r][c] - '0' );
             }
         }
-
         return true;
-
-
-
-
     }
 };
